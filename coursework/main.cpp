df8b8a2df87000cb04edc5293fe00d78bfe1668a
tomijon <thomas27johnson39@gmail.com>
2025-02-13 14:02:17 +0000

    Update main method.

--- coursework/main.cpp
+++ coursework/main.cpp
@@ -2,7 +2,7 @@
 #include "Renderer.h"
 
 int main() {
-	Window w("Coursework!", 1920, 1080, false);
+	Window w("Coursework!", 600, 600, false);
 	if (!w.HasInitialised()) {
 		return -1;
 	}
@@ -18,28 +18,28 @@ int main() {
 	while (w.UpdateWindow() && !Window::GetKeyboard()->KeyDown(KEYBOARD_ESCAPE)) {
 		float timestep = w.GetTimer()->GetTimeDeltaSeconds();
 		renderer.getAnimator()->update(w.GetTimer()->GetTotalTimeSeconds(), timestep);
-		renderer.UpdateScene(timestep);
+		renderer.UpdateScene(timestep); 
 		renderer.RenderScene();
 		renderer.SwapBuffers();
-		if (Window::GetKeyboard()->KeyDown(KEYBOARD_F5)) {
+		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_F5)) {
 			Shader::ReloadAllShaders();
 		}
-		if (Window::GetKeyboard()->KeyDown(KEYBOARD_P)) {
+		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_P)) {
 			renderer.applyTimeSkip();
 		}
-		if (Window::GetKeyboard()->KeyDown(KEYBOARD_1)) {
+		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_1)) {
 			renderer.toggleBloom();
 		}
-		if (Window::GetKeyboard()->KeyDown(KEYBOARD_2)) {
+		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_2)) {
 			renderer.toggleBlackAndWhite();
 		}
-		if (Window::GetKeyboard()->KeyDown(KEYBOARD_3)) {
+		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_3)) {
 			renderer.toggleBlackAndWhiteSmooth();
 		}
-		if (Window::GetKeyboard()->KeyDown(KEYBOARD_4)) {
+		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_4)) {
 			renderer.toggleSmooth();
 		}
-		if (Window::GetKeyboard()->KeyDown(KEYBOARD_5)) {
+		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_5)) {
 			renderer.toggleBlur();
 		}
 	}
